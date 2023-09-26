2874c93a8f491831f11c5336ad9a5677f52f33c7
SYAQIRANAZ <144090657+SYAQIRANAZ@users.noreply.github.com>
2023-09-26 13:04:20 +0700

    Add files via upload

---
+++ PemilihanPercobaan128.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan128 {
+    public static void main(String[] args) {
+        Scanner input28 = new Scanner(System.in);
+        System.out.println("Masukkan Angka: ");
+        int angka = input28.nextInt();
+        
+    if (angka % 2 == 0) 
+        System.out.println("Angka "+angka+" bilagan genap");
+    else
+        System.out.println("Angka "+angka+" bilangan ganjil");
+    }
+        
+        
+     }
+
